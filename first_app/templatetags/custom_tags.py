6419140b2bc503803e19e6334d1bcd85d40afae9
satya kusampudi <sr@satyas-MacBook-Pro.local>
2021-03-31 12:15:46 +0530

    commiting latest changes in Inclusion tags(working)

--- first_app/templatetags/custom_tags.py
+++ first_app/templatetags/custom_tags.py
@@ -1,6 +1,9 @@
 from django import template
+from django.contrib.auth import get_user_model
+from django.template.loader import get_template
 register = template.Library()
-
+from datetime import datetime
+from first_app.models import Department
 
 def modify_name(value, args):
     if args == 'first_name':
@@ -16,6 +19,20 @@ def define_number(value, args):
     if args == 'triple':
         return value*3
     return value
+
+@register.simple_tag
+def current_time(format_string):
+    return datetime.now().strftime(format_string)
+
+# Depart = get_user_model()
+
+
+
+@register.inclusion_tag('first_app/latest.html')
+def show_depart_table():
+    departs = Department.objects.all()
+    return {'departments': departs}
+
 
 
 register.filter('modify_name', modify_name)
